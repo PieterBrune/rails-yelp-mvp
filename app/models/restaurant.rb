4e5a9c03c529e17901a5e173f4b4cb7e34d67a94
pieter brune <pebrune@gmail.com>
2021-11-24 13:31:40 +0800

    Created restaurant and review models with validation

---
+++ app/models/restaurant.rb
@@ -0,0 +1,13 @@
+class Restaurant < ApplicationRecord
+  def self.categories
+    %w[chinese italian japanese french belgian]
+  end
+
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+
+  validates :address, presence: true
+
+  validates :category, inclusion: { in: categories }  # , message: '%<value> is not a valid category'
+end
